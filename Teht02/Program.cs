a221a5eaab742d1064edc0a444063d822c5a07ea
K2038 <K2038@OHJE-330-WS-09>
2017-01-19 13:34:54 +0200

    Add project files.

---
+++ Teht02/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arvosana;
+            Console.Write("Anna arvosana > ");
+            string line = Console.ReadLine();
+            arvosana = int.Parse(line);
+
+            switch (arvosana)
+            {
+
+                case 0:
+                case 1:
+                    Console.WriteLine("Sait arvosanan 0!");
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("Sait arvosanan 1!");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("Sait arvosanan 2!");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Sait arvosanan 3!");
+                    break;
+                case 8:
+                case 9:
+                    Console.WriteLine("Sait arvosanan 4!");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("Sait arvosanan 5!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Teht03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2, number3;
+            Console.Write("Give first number < ");
+            string line = Console.ReadLine(); //read input
+            number1 = int.Parse(line); // parse input "1" -> 1
+
+            Console.Write("Give second number < ");
+            line = Console.ReadLine(); //read input
+            number2 = int.Parse(line); // parse input "2" -> 2
+
+            Console.Write("Give third number < ");
+            line = Console.ReadLine(); //read input
+            number3 = int.Parse(line); // parse input "3" -> 3
+
+            Console.Write("The sum is: " + (number1 + number2 + number3) + Environment.NewLine);
+
+
+            Console.Write("The average is: " + ((number1 + number2 + number3) / 3) + Environment.NewLine);
+
+            Console.ReadLine();
+        }
+    }
+}
