dcbd66fd153ae2a842ef127e809af92a831038d4
K2038 <K2038@student.jamk.fi>
2017-01-26 09:13:37 +0200

    Pretty much all done expect hangman...

---
+++ Teht19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (nothing to see = yes) {
+                do.nothing;
+            }
+
+            else (nothing to really see, really...){
+                do.absolutenothing;
+            }
+        }
+    }
+}
---
+++ Teht20/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ownage;
+            int womage;
+
+            Console.Write("Give your age << ");
+            string line = Console.ReadLine();
+            bool result1 = int.TryParse(line, out ownage);
+
+            Console.Write("Give age of woman << ");
+            line = Console.ReadLine();
+            bool result2 = int.TryParse(line, out womage);
+
+            if (result1 && result2 == true)
+            {
+
+                if (womage - ownage >= 10)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Woman is a cougar, good luck.");
+                    Console.WriteLine(@"                  /  __  \ ");
+                    Console.WriteLine(@" / \ ----/ \     / /    \ \");
+                    Console.WriteLine(@"                 \/      \ \");
+                    Console.WriteLine("  < >   < >              | |");
+                    Console.WriteLine(@"\     ^     /------------| |");
+                    Console.WriteLine(@"  \  -^-  /        ____    |");
+                    Console.WriteLine(@" |   ---          /    \   |");
+                    Console.WriteLine(" |                      |  |");
+                    Console.WriteLine(@"  \--  \         /------|  /");
+                    Console.WriteLine("   --------------_________/");
+                    Console.WriteLine();
+
+                }
+
+                else
+                {
+                    Console.WriteLine("\nUnfortunately woman is not a cougar, look elsewhere.\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input!");
+            }
+        }
+    }
+}
